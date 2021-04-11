22c47abebe409a34f5dfd78970dd6600a2cb7092
Anas Al-Ramahi <anas_alramahi@outlook.com>
2021-04-11 16:02:45 +0300

    added Paris

--- javascript/app.js
+++ javascript/app.js
@@ -162,3 +162,58 @@ for(let i=0; i< Dubai.cookiesThroughTheDayForLocation.length; i++){
 li = document.createElement('li');
 ul.appendChild(li);
 li.textContent = `Total: ${Dubai.sumOfCookies} cookies`;
+
+
+
+const Paris = {
+    name: 'Paris',
+    minCookiesPerHour: 20,
+    maxCookiesPerHour: 38,
+    avgCookiesPerCustomer: 2.3,
+    cookiesThroughTheDayForLocation: [],
+    sumOfCookies: 0,
+
+    numOfCustomersPerHour: function(min,max){
+        return Math.floor(Math.random() * ( max - min + 1 ) + min );
+    },
+    calcCookiesPerHour: function(){
+        for(let i=0; i<14; i++){
+            let number = Math.floor(this.numOfCustomersPerHour(this.minCookiesPerHour, this.maxCookiesPerHour) * this.avgCookiesPerCustomer);
+            this.cookiesThroughTheDayForLocation.push(number);
+            this.sumOfCookies += number;
+        }
+    },
+
+    
+}
+Paris.calcCookiesPerHour();
+console.log(Paris.cookiesThroughTheDayForLocation);
+console.log(Paris.sumOfCookies);
+
+ section = document.getElementById("first");
+console.log(section);
+h2 = document.createElement('h2');
+console.log(h2);
+section.appendChild(h2);
+h2.textContent = Paris.name;
+
+ul = document.createElement('ul');
+section.appendChild(ul);
+
+li;
+time = 6;
+pm = "false";
+for(let i=0; i< Paris.cookiesThroughTheDayForLocation.length; i++){
+    li = document.createElement('li');
+    ul.appendChild(li);
+    if( i <= 6 ){
+        li.textContent = `${time}am: ${Paris.cookiesThroughTheDayForLocation[i]} cookies`;
+    }else {
+        li.textContent = `${time}pm: ${Paris.cookiesThroughTheDayForLocation[i]} cookies`;
+    }
+    time = time%12;
+    time++;
+}
+li = document.createElement('li');
+ul.appendChild(li);
+li.textContent = `Total: ${Paris.sumOfCookies} cookies`;
