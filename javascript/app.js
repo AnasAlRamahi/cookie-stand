894fe46b52470d83b556d6eedbb7c79891509ec3
Anas Al-Ramahi <anas_alramahi@outlook.com>
2021-04-11 15:58:11 +0300

    added Tokyo

--- javascript/app.js
+++ javascript/app.js
@@ -51,6 +51,58 @@ for(let i=0; i< Seattle.cookiesThroughTheDayForLocation.length; i++){
 }
 li = document.createElement('li');
 ul.appendChild(li);
-li.textContent = `Total: ${Seattle.sumOfCookies} cookies`
+li.textContent = `Total: ${Seattle.sumOfCookies} cookies`;
 
 
+const Tokyo = {
+    name: 'Tokyo',
+    minCookiesPerHour: 3,
+    maxCookiesPerHour: 24,
+    avgCookiesPerCustomer: 1.2,
+    cookiesThroughTheDayForLocation: [],
+    sumOfCookies: 0,
+
+    numOfCustomersPerHour: function(min,max){
+        return Math.floor(Math.random() * ( max - min + 1 ) + min );
+    },
+    calcCookiesPerHour: function(){
+        for(let i=0; i<14; i++){
+            let number = Math.floor(this.numOfCustomersPerHour(this.minCookiesPerHour, this.maxCookiesPerHour) * this.avgCookiesPerCustomer);
+            this.cookiesThroughTheDayForLocation.push(number);
+            this.sumOfCookies += number;
+        }
+    },
+
+    
+}
+Tokyo.calcCookiesPerHour();
+console.log(Tokyo.cookiesThroughTheDayForLocation);
+console.log(Tokyo.sumOfCookies);
+
+ section = document.getElementById("first");
+console.log(section);
+h2 = document.createElement('h2');
+console.log(h2);
+section.appendChild(h2);
+h2.textContent = Tokyo.name;
+
+ul = document.createElement('ul');
+section.appendChild(ul);
+
+li;
+time = 6;
+pm = "false";
+for(let i=0; i< Tokyo.cookiesThroughTheDayForLocation.length; i++){
+    li = document.createElement('li');
+    ul.appendChild(li);
+    if( i <= 6 ){
+        li.textContent = `${time}am: ${Tokyo.cookiesThroughTheDayForLocation[i]} cookies`;
+    }else {
+        li.textContent = `${time}pm: ${Tokyo.cookiesThroughTheDayForLocation[i]} cookies`;
+    }
+    time = time%12;
+    time++;
+}
+li = document.createElement('li');
+ul.appendChild(li);
+li.textContent = `Total: ${Tokyo.sumOfCookies} cookies`;
